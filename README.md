427f60b38edcac45b6043b84f00a3f3c74d1b4ff
mfoman <frederikblunck@gmail.com>
2022-10-01 13:04:54 +0200

    protos

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # README
 
 https://github.com/grpc-ecosystem/grpc-gateway
+
+## The grpc
+
+protoc -I . \
+    --go_out ./gen/go/ --go_opt paths=source_relative \
+    --go-grpc_out ./gen/go/ --go-grpc_opt paths=source_relative \
+    your/service/v1/your_service.proto
+
+## Just the gateway stubs
+
+protoc -I . --grpc-gateway_out ./gen/go \
+    --grpc-gateway_opt logtostderr=true \
+    --grpc-gateway_opt paths=source_relative \
+    --grpc-gateway_opt generate_unbound_methods=true \
+    your/service/v1/your_service.proto
+
+buf mod update
+
+## Annotations
+
+https://github.com/grpc-ecosystem/grpc-gateway/blob/master/examples/internal/proto/examplepb/a_bit_of_everything.proto
--- main.go
+++ main.go
@@ -1,11 +1,47 @@
-//go:build tools
-// +build tools
-
-package tools
+package main
 
 import (
-	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway"
-	_ "github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2"
-	_ "google.golang.org/grpc/cmd/protoc-gen-go-grpc"
-	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
+	"context"
+	"flag"
+	"net/http"
+
+	"github.com/golang/glog"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	gw "github.com/mfoman/grpc-gateway/service" // Update
 )
+
+var (
+  // command-line options:
+  // gRPC server endpoint
+  grpcServerEndpoint = flag.String("grpc-server-endpoint",  "localhost:9090", "gRPC server endpoint")
+)
+
+func run() error {
+  ctx := context.Background()
+  ctx, cancel := context.WithCancel(ctx)
+  defer cancel()
+
+  // Register gRPC server endpoint
+  // Note: Make sure the gRPC server is running properly and accessible
+  mux := runtime.NewServeMux()
+  opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
+  err := gw.RegisterYourServiceHandlerFromEndpoint(ctx, mux,  *grpcServerEndpoint, opts)
+  if err != nil {
+    return err
+  }
+
+  // Start HTTP server (and proxy calls to gRPC server endpoint)
+  return http.ListenAndServe(":8081", mux)
+}
+
+func main() {
+  flag.Parse()
+  defer glog.Flush()
+
+  if err := run(); err != nil {
+    glog.Fatal(err)
+  }
+}
